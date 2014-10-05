2c67022d57b30600fe2be8fce7fe6c8f1dc9f982
mrbarbasa <mrbarbasa@gmail.com>
2014-10-04 15:38:50 -1000

    Finish exercise

--- elevator.js
+++ elevator.js
@@ -3,4 +3,17 @@
 
 module.exports = {
 
+  goto: function(currentFloor, destFloor) {
+    var result = 0;
+    if (typeof currentFloor === 'number' && typeof destFloor === 'string') {
+      currentFloor = Math.floor(currentFloor);
+      destFloor = Math.floor(Number(destFloor));
+
+      if (currentFloor <=3 && currentFloor >= 0 && destFloor <= 3 && destFloor >= 0) {
+        result = destFloor - currentFloor;
+      }
+    }
+    return result;
+  }
+
 };
